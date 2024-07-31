f71720703a17685792b912da4c10a3d88afd19a3
seele-ai <126869991+seele-ai@users.noreply.github.com>
2024-08-01 01:04:36 +0800

    Add files via upload

---
+++ js/top5_sub.js
@@ -0,0 +1,50 @@
+var top5_sub = {
+    "08-18" : {
+        "in" : {"x":["Physics",                          
+            "Astronomy_Astrophysics",              
+            "Instruments_Instrumentation",          
+            "Genetics_Heredity",                    
+            "Biochemistry_MolecularBiology"],
+            "y" : [333872,312638,45632,21363,20728]
+        },
+        "out" : {"x" : ["Physics",
+            "Astronomy_Astrophysics",
+            "Instruments_Instrumentation",
+            "Genetics_Heredity",
+            "Biochemistry_MolecularBiology"],
+            "y" : [333479,313508,44926,20355,19819]
+        }
+    },
+    "09-13" : {
+        "in" : {"x" : ["Physics",
+            "Astronomy_Astrophysics",
+            "Instruments_Instrumentation",
+            "Biochemistry_MolecularBiology",
+            "Genetics_Heredity",],
+            "y" : [129276,119521,17174,8027,7457]
+        },
+        "out" : {"x" : ["Physics",
+            "Astronomy_Astrophysics",
+            "Instruments_Instrumentation",
+            "Biochemistry_MolecularBiology",
+            "Genetics_Heredity"],
+            "y" : [128327,121011,16665,7834,7827]
+        }
+    },
+    "14-18" : {
+        "in" : {"x" : ["Physics",
+            "Astronomy_Astrophysics",
+            "Instruments_Instrumentation",
+            "Genetics_Heredity",
+            "Oncology"],
+            "y" : [198255,187643,27483,10512,10094]
+        },
+        "out" : {"x" : ["Physics",
+            "Astronomy_Astrophysics",
+            "Instruments_Instrumentation",
+            "Genetics_Heredity",
+            "Oncology"],
+            "y" : [199096,186535,27434,10568,10141]
+        }
+    }
+}
